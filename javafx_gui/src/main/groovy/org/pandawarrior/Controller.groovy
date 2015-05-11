6a7ed5a64573a25128b649e2718d77762074c73e
Liew Jun Tung <pandawarrior91@gmail.com>
2015-05-12 04:06:56 +0800

    new project structure

---
+++ javafx_gui/src/main/groovy/org/pandawarrior/Controller.groovy
@@ -0,0 +1,101 @@
+package org.pandawarrior
+
+import javafx.event.ActionEvent
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.paint.Color
+import javafx.stage.DirectoryChooser
+import javafx.stage.FileChooser
+import javafx.stage.Stage
+
+import org.pandawarrior.ReadXml
+import org.pandawarrior.WriteXml
+
+/**
+ * Created by jt on 5/10/15.
+ */
+class Controller {
+
+    public TextField x2c_txtXMLFolder
+    public TextField x2c_txtCSVFile
+    public TextField c2x_txtCSVFile
+    public TextField c2x_txtXMLFolder
+    public Label c2x_indicator
+    public Label x2c_indicator
+
+    /**
+     * XML to CSV
+     * @param actionEvent
+     */
+    public void x2c_openXMLFolder(ActionEvent actionEvent) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select folder");
+        File folder = chooser.showDialog(new Stage());
+        x2c_txtXMLFolder.setText folder.absolutePath
+    }
+
+    public void x2c_saveDestination(ActionEvent actionEvent) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open File");
+        chooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("CSV File (*.csv)", "*.csv")
+        )
+        chooser.setInitialFileName("new.csv")
+        File file = chooser.showSaveDialog(new Stage());
+        x2c_txtCSVFile.setText "${file.absolutePath}"
+
+    }
+
+    public void x2c_runXMLtoCSV(ActionEvent actionEvent) {
+        String folderPath = x2c_txtXMLFolder.getText()
+        String filePath = x2c_txtCSVFile.getText()
+        if (folderPath.length() > 0 && filePath.length() > 0) {
+            boolean result = ReadXml.parse(folderPath, filePath)
+            if (!result) {
+                x2c_indicator.setText("UNSUCESSFUL!")
+                x2c_indicator.setTextFill(Color.RED)
+            } else {
+                x2c_txtCSVFile.clear()
+                x2c_txtXMLFolder.clear()
+                x2c_indicator.setText("SUCCESS!")
+                x2c_indicator.setTextFill(Color.GREEN)
+            }
+        } else {
+            x2c_indicator.setText("Please set folder or file location!")
+            x2c_indicator.setTextFill(Color.RED)
+        }
+    }
+
+    public void c2x_openCSVFile(ActionEvent actionEvent) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open File");
+        chooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("CSV File (*.csv)", "*.csv")
+        )
+        File file = chooser.showOpenDialog(new Stage());
+        c2x_txtCSVFile.setText "${file.absolutePath}"
+
+    }
+
+    public void c2x_saveDestination(ActionEvent actionEvent) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select folder");
+        File folder = chooser.showDialog(new Stage());
+        c2x_txtXMLFolder.setText folder.absolutePath
+    }
+
+    public void c2x_runCSVtoXML(ActionEvent actionEvent) {
+        boolean result = WriteXml.parse(c2x_txtCSVFile.getText(), c2x_txtXMLFolder.getText())
+        if (!result) {
+            c2x_indicator.setText("UNSUCESSFUL!")
+            c2x_indicator.setTextFill(Color.RED)
+        } else {
+            c2x_txtCSVFile.clear()
+            c2x_txtXMLFolder.clear()
+            c2x_indicator.setText("SUCCESS!")
+            c2x_indicator.setTextFill(Color.GREEN)
+        }
+    }
+
+
+}
---
+++ xml_csv_parse/src/main/groovy/org/pandawarrior/WriteXml.groovy
@@ -0,0 +1,90 @@
+package org.pandawarrior
+
+import groovy.xml.MarkupBuilder
+
+/**
+ * Created by jt on 5/9/15.
+ */
+class WriteXml {
+
+    static boolean parse(String csvPath, String moduleFolder) {
+        //load and split the file
+        List rows = getRows(csvPath)
+        List head = getHead(rows)
+        Map mainDict = getMainMap(rows, head)
+        Map transDict = getTransMap(rows)
+        println mainDict
+        return writeFile(moduleFolder, mainDict, transDict)
+    }
+
+    protected static List getRows(String csvPath){
+        String csv = new File(csvPath).getText()
+        String[] lines = csv.split('\n')
+        List<String[]> rows = lines.collect { it.split(',') }
+        return rows
+    }
+
+    protected static List getHead(List rows){
+        List head = rows.get(0)
+        head = head - "name"
+        head = head - "translatable"
+        return head
+    }
+
+    protected static Map getTransMap(List rows){
+        Map transMap = [:]
+        for (int i=1; i<rows.size(); i++){
+            transMap[rows[i][0]] = rows[i][1].replaceAll("\\s", "")
+        }
+        return transMap
+    }
+
+    protected static Map getMainMap(List rows, List head){
+        Map mainDict = [:]
+
+        for (int i = 0; i < head.size(); i++) {
+            def tempMap = [:]
+            for (int j = 1; j < rows.size(); j++) {
+                def column = rows.get(j)
+                def name = column[0]
+                tempMap[name] = column[i + 2]
+            }
+            mainDict[head[i].replaceAll("\\s", "")] = tempMap
+        }
+        return mainDict
+    }
+
+    private static boolean writeFile(String destination, Map mainDict, Map transDict) {
+        mainDict.each {
+            def stringWriter = new StringWriter()
+            def xml = new MarkupBuilder(stringWriter)
+            def mainDictValue = it.value
+            def fileName = it.key
+            String dir = "${destination}/res/${it.key}/"
+            File file = new File(dir, 'strings.xml')
+            File folder = new File(dir)
+
+            if (!folder.exists()) {
+                folder.mkdirs()
+            }
+            xml.resources {
+
+                mainDictValue.each {
+                    def key = it.key
+                    def value = it.value.replaceAll("@@", ",")
+
+                    if (fileName.equals("values") && transDict[key].equals("false")){
+                        string(name: key, translatable:transDict[key], value)
+                    }else if (transDict[key].equals("true") && !value.equals("null")){
+                        string(name: key, value)
+                    }
+                }
+            }
+            def records = stringWriter.toString()
+            file.withWriter('utf-8') { writer ->
+                writer.write(records)
+            }
+            return true
+        }
+    }
+}
